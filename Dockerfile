8dc9c61456c4d822b5ae15bc69db896e192e7abc
Tim Dettrick <t.dettrick@uq.edu.au>
2015-01-15 15:39:44 +1000

    Registering containers for service discovery

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ MAINTAINER t.dettrick@uq.edu.au
 
 # Set defaults which should be overridden on run
 ENV CONFIG_DIR /opt/config
+ENV ETCD_VERSION v2.0.0_rc.1
+ENV SERVICE_DISCOVERY_PATH /dit4c/containers
 
 RUN yum install -y curl docker
 
--- opt/run.sh
+++ opt/run.sh
@@ -6,6 +6,7 @@ echo "All config files should be in $CONFIG_DIR"
 echo "Etcd for Hipache should be configured in dit4c-highcommand.conf"
 
 DOCKER_SOCKET="/var/run/docker.sock"
+ETCD_IMAGE="quay.io/coreos/etcd:$ETCD_VERSION"
 
 if [ ! -S $DOCKER_SOCKET ]
 then
@@ -13,11 +14,28 @@ then
     exit 1
 fi
 
+if [[ $HOST == "" ]]
+then
+    echo "HOST should be specified as an environment variable"
+    exit 1
+fi
+
+if [[ $(docker inspect -f "{{ .Image }}" dit4c_etcd) == "" ]]
+then
+    echo "Container \"dit4c_etcd\" must exist."
+    exit 1
+fi
+
+ETCD_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_etcd)
+ETCDCTL_CMD="docker run --rm -e ETCDCTL_PEERS=$ETCD_IP:2379 --entrypoint /etcdctl $ETCD_IMAGE --no-sync"
+
 # Create DB server
 docker start dit4c_couchdb || docker run -d --name dit4c_couchdb \
     -v /usr/local/var/log/couchdb:/var/log \
     --restart=always \
     klaemo/couchdb
+$ETCDCTL_CMD set "$SERVICE_DISCOVERY_PATH/dit4c_couchdb/$HOST" \
+  $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_couchdb)
 
 # Wait a little to ensure dit4c_couchdb exists
 until [[ `docker inspect dit4c_{etcd,couchdb}; echo $?` ]]
@@ -32,5 +50,7 @@ docker start dit4c_highcommand || docker run -d --name dit4c_highcommand \
     --link dit4c_etcd:etcd \
     --link dit4c_couchdb:couchdb \
     dit4c/dit4c-platform-highcommand
+$ETCDCTL_CMD set "$SERVICE_DISCOVERY_PATH/dit4c_highcommand/$HOST" \
+  $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_highcommand)
 
 echo "Done configuring portal."
