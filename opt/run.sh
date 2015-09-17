9c4a3349d23f2b456f4b4dfc7e09921d57d25b8b
Tim Dettrick <t.dettrick@uq.edu.au>
2015-09-18 09:15:20 +1000

    Updating to stand alone from routing

--- opt/run.sh
+++ opt/run.sh
@@ -22,12 +22,6 @@ then
     exit 1
 fi
 
-if [[ $(docker inspect -f "{{ .Image }}" dit4c_etcd) == "" ]]
-then
-    echo "Container \"dit4c_etcd\" must exist."
-    exit 1
-fi
-
 docker start -ia dit4c_ssl_keys || docker run --name dit4c_ssl_keys \
   -v $SSL_DIR/server.key:/etc/ssl/server.key \
   -v $SSL_DIR/server.crt:/etc/ssl/server.crt \
@@ -47,27 +41,25 @@ else
     exit 1
 fi
 
-ETCD_IP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_etcd)
+ETCD_IP=$(ip route show | grep -Eo "via \S+" | cut -d " " -f 2)
 ETCDCTL_CMD="docker run --rm -e ETCDCTL_PEERS=$ETCD_IP:2379 --entrypoint /etcdctl $ETCD_IMAGE --no-sync"
 
 # Create DB server
 docker start dit4c_couchdb || docker run -d --name dit4c_couchdb \
-    -v /usr/local/var/log/couchdb:/var/log \
     --restart=always \
     klaemo/couchdb
 $ETCDCTL_CMD set "$SERVICE_DISCOVERY_PATH/dit4c_couchdb/$HOST" \
   $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" dit4c_couchdb)
 
 # Wait a little to ensure dit4c_couchdb exists
-until [[ `docker inspect dit4c_{etcd,couchdb}; echo $?` ]]
+while true
 do
-    sleep 1
+  docker inspect dit4c_couchdb > /dev/null && break || sleep 1
 done
 
 # Create highcommand server
 docker start dit4c_highcommand || docker run -d --name dit4c_highcommand \
     -v $CONFIG_DIR/dit4c-highcommand.conf:/etc/dit4c-highcommand.conf \
-    --link dit4c_etcd:etcd \
     --link dit4c_couchdb:couchdb \
     dit4c/dit4c-platform-highcommand
 $ETCDCTL_CMD set "$SERVICE_DISCOVERY_PATH/dit4c_highcommand/$HOST" \
